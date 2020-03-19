31f9fc0a58b4860278b50a22b26f3e201ce182e4
Shijun Deng <dengshijun233@gmail.com>
2020-03-19 15:39:13 -0400

    added validation for user info like email format, password match...

--- functions/index.js
+++ functions/index.js
@@ -39,6 +39,17 @@ app.get('/screams', (req, res) => {
     .catch(err => console.error(err));
 });
 
+const isEmpty = string => {
+  if (string.trim() == '') return true;
+  else return false;
+};
+
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
 app.post('/scream', (req, res) => {
   const newScream = {
     body: req.body.body,
@@ -66,7 +77,23 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle
   };
 
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Email must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty';
+  if (newUser.password !== newUser.confirmPassword)
+    errors.confirmPassword = 'Password must match';
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+
   //TODO: validate data
+  let token, userId;
   db.doc(`/users/${newUser.handle}`)
     .get()
     .then(doc => {
@@ -79,13 +106,27 @@ app.post('/signup', (req, res) => {
       }
     })
     .then(data => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
       console.error(err);
+      if (err.code === 'auth/email-already-in-use') {
+        return res.status(400).json({ email: 'Email is already in use' });
+      }
       return res.status(500).json({ error: err.code });
     });
 });
