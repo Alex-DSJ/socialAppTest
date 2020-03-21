66f6134e52127dab045b06b0a2e88a241fd34b83
Shijun Deng <dengshijun233@gmail.com>
2020-03-20 22:34:43 -0400

    add validation for signup&login and re-organized folders&files

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyCq5OAibtqvauOSQqkzaNFkrbkDf_0pTs8',
+  authDomain: 'socialaptest.firebaseapp.com',
+  databaseURL: 'https://socialaptest.firebaseio.com',
+  projectId: 'socialaptest',
+  storageBucket: 'socialaptest.appspot.com',
+  messagingSenderId: '490349029455',
+  appId: '1:490349029455:web:d1891d041fd5e26339e23a',
+  measurementId: 'G-DC57M4NSYS'
+};
